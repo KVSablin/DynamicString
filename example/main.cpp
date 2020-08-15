9d61314ade74b736cc7afd77941a8291eeb0ebf5
Konstantin <58115062+TOPTcpp@users.noreply.github.com>
2020-08-16 01:53:47 +0500

    add example

---
+++ example/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <DynamicString>
+
+int main(int argc, char** argv)
+{
+    if (argc > 1)
+    {
+        std::vector<String> list;
+        list.reserve(argc - 1);
+        list.assign(argv + 1, argv + argc);
+        for(auto iter = list.rbegin(); iter != list.rend(); ++iter)
+        {
+            String& str = *iter;
+            for (int i = str.length() - 1; i >= 0; --i)
+                std::cout << str[i];
+             std::cout << ' ';
+        }
+        std::cout << '\n';
+    }
+    else
+    {
+        std::cout << String("No input arguments") << '\n';
+    }
+   
+    return 1;
+}
